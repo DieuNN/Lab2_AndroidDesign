20e6aa2b05edcd949614ebcf2e95c3d744ccd40a
DieuNN <nongngocdieu20122002@gmail.com>
2021-06-28 20:53:42 +0700

    Final commit

--- app/src/main/java/com/example/lab2/Bai1.kt
+++ app/src/main/java/com/example/lab2/Bai1.kt
@@ -1,11 +1,28 @@
 package com.example.lab2
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Bai1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bai1)
+
+        var text:TextView = findViewById(R.id.lblBai1)
+        text.text = "Fpoly"
+        val font:Typeface = Typeface.createFromAsset(assets, "Blazed.ttf")
+        text.typeface = font
+
+
+
+
+
+
+
+
     }
+
+
 }
